645e05d13b995fc9483cc931095ad1039c860ac8
mgwizdz <mgwizdz@future-processing.com>
2022-05-27 15:42:14 +0200

    create message

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+
   // process.exit();
   res.setHeader('Content-Type', 'text/html');
+
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    const messageHtml = fs.readFileSync('message.html', 'utf-8');
+    res.write(messageHtml);
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+
   const homeHtml = fs.readFileSync('home.html', 'utf-8');
   res.write(homeHtml);
 });
